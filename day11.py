3bc78aec0af4f8ea686ec2e8f21a3db415bc56f5
Indrajit <indram2026@gmail.com>
2026-06-18 19:25:18 +0530

    Day 11 - Practised 6 questions based on patterns and nested loops.

---
+++ day11.py
@@ -0,0 +1,42 @@
+for i in range(1,5):
+    for j in range(4):
+        print("*", end="")
+print()
+
+
+for i in range(1,5):
+    for j in range(i):
+        print("*", end="")
+    print()
+
+
+for i in range(1,5):
+    for j in range(5-i):
+        print(" ", end="")
+    for j in range(i):
+        print("*", end="")
+    print()
+
+
+for i in range(5,0, -1):
+    for j in range(i):
+        print("*", end="")
+    print()
+
+for i in range(1,5):
+    for j in range(1, i + 1):
+        print(j, end="")
+    print()
+
+
+for i in range(1,5):
+    for j in range(4 - i, 0, -1):
+        print(" ", end=" ")
+
+    for j in range(1, i + 1):
+        print(j, end=" ")
+
+    for j in range(i - 1, 0, -1):
+        print(j, end=" ")
+    print()
+
