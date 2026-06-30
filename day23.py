ba8109f08847f52b912d802c2a375cf86bb32368
Indrajit <indram2026@gmail.com>
2026-06-30 20:19:52 +0530

    Day 23: Created a working ECG classifier.

--- day23.py
+++ day23.py
@@ -5,7 +5,7 @@ qt_interval = int(input("Please input your QT interval: "))
 def classify_ecg(bpm,qt_interval):
     if bpm > 100:
         bpm_class = "Tachycardia"
-    elif bpm <= 100 and bpm >= 6:
+    elif bpm <= 100 and bpm >= 60:
         bpm_class = "Normal"
     else:
         bpm_class = "Bradycardia"
@@ -25,7 +25,7 @@ def classify_ecg(bpm,qt_interval):
         risk = "Medium"
     
 
-    return {"Classification": {bpm_class}, "Heart Rate": {bpm}, "QT Interval": {qt_interval}, "QT Class": {qt_class}, "Risk Factor": {risk}}
+    return {"Classification": bpm_class, "Heart Rate": bpm, "QT Interval": qt_interval, "QT Class": qt_class, "Risk Factor": risk}
 
 result = classify_ecg(bpm, qt_interval)
 print(result)
